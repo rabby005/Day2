365783eb1c159ed012df0d167c34a20ad28eda1c
rabby005 <farabyabdulla@gmail.com>
2025-12-29 13:55:23 +0600

    len

--- Lenth.py
+++ Lenth.py
@@ -10,15 +10,18 @@ print(len(str2))
 #or 
 
 str3="fidora"
-str4="freesystem"
+str4="free"
 str5=len(str3)
 str6=len(str4)
+print(str5)
+print(str6)
 
 #or
 
 stra="fidora"
 strb="freesystem"
 strc=len(stra),len(strb)
+print(strc)
 
 #or "" use kore maje space dile tar length ber kora jay
 d="faraby"
