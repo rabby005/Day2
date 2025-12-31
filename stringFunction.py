fe7dc23f6c69232e564268a73258ee156e1586b5
rabby005 <farabyabdulla@gmail.com>
2025-12-31 13:58:18 +0600

    Strfunction

--- stringFunction.py
+++ stringFunction.py
@@ -1,11 +1,14 @@
+
+
 #String Functions
+
+
 #str = "I am a coder."
-#str.endswith("er.") #returns true if string ends with substr
-#str. capitalize() #capitalizes 1st char
-#str.replace old, new) #replaces all occurrences of old with paradfor
-#str.find (word )
-#returns 1st index of 1st occurrence
-#str.count("am") #counts the occurrence of substr in string
+#str.endswith("er.")        returns true if string ends with substr
+#str. capitalize()          capitalizes 1st char
+#str.replace old, new)      replaces all occurrences of old with paradfor
+#str.find (word )           returns 1st index of 1st occurrence
+#str.count("am")            counts the occurrence of substr in string
 
 
 str = "I am a coder. I love to code."
