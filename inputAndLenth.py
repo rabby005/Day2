f8e1ed964f513ccbb4a99387f358e2795d397550
rabby005 <farabyabdulla@gmail.com>
2026-01-05 13:45:31 +0600

    inputLen

--- inputAndLenth.py
+++ inputAndLenth.py
@@ -4,3 +4,6 @@ print("name length",len(name))
 
 age = input("enter your age here : ")
 print("age length",len(age))
+
+friend = input("Enter your friend name here : ")
+print("friend name length",len(friend))
