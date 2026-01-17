7c2f4690682a1f19d99efb212a88380800000b6e
rabby005 <farabyabdulla@gmail.com>
2026-01-17 04:55:44 -0500

    day2

--- StudentMarkeCondition.py
+++ StudentMarkeCondition.py
@@ -14,7 +14,9 @@ elif(mark < 90 and mark >= 80):
     print("Grade B")
 elif(mark < 80 and mark >= 70):
     print("Grade C")
-elif(mark < 70):
+elif(mark < 70 and mark >= 60):
+    print("Grade D")
+elif(mark < 60 and mark >= 50):
     print("Grade D")
 elif(mark < 60):
     print("Grade F")
