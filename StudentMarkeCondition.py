a043e5430eb480d8160dd6eb0b96890fe600835f
rabby005 <farabyabdulla@gmail.com>
2026-01-16 20:32:51 -0500

    day2

---
+++ StudentMarkeCondition.py
@@ -0,0 +1,14 @@
+# qusrion 1 
+# Grade students based on marks
+# marks >= 90, grade = "A"
+# 90 > marks >= 80, grade = "B"
+# 80 > marks >= 70, grade = "C"
+# 70 > marks, grade = "D"
+
+
+mark = 30
+
+if(mark >= 90):
+    print("Grade A")
+elif(mark < 90 and mark >= 80):
+    print("Grade B")
