e21b712df935fb7b2e4eacaa9453e207c7efef90
rabby005 <farabyabdulla@gmail.com>
2026-01-17 04:57:53 -0500

    day2

--- StudentMarkeCondition.py
+++ StudentMarkeCondition.py
@@ -20,4 +20,23 @@ elif(mark < 60 and mark >= 50):
     print("Grade E")
 elif(mark < 50 and mark >= 40):
     print("Grade F")
+    
+    
+    
+    # 
+
+mark = (int(input("Enter your mark: ")))
+
+if(mark >= 90):
+    print("Grade A")
+elif(mark < 90 and mark >= 80):
+    print("Grade B")
+elif(mark < 80 and mark >= 70):
+    print("Grade C")
+elif(mark < 70 and mark >= 60):
+    print("Grade D")
+elif(mark < 60 and mark >= 50):
+    print("Grade E")
+elif(mark < 50 and mark >= 40):
+    print("Grade F")
 
