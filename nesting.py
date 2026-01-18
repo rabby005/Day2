a03c89d4eb6d822573c518003eaa0ef4916ad1e9
rabby005 <farabyabdulla@gmail.com>
2026-01-18 12:18:06 +0600

    day2nested

--- nesting.py
+++ nesting.py
@@ -6,3 +6,5 @@ if(age >= 18):
         print("You are eligible for retirement benefits.")
 else:
   print("You are kid and not able to all benifits.")
+  
+#   eitatke nestet vola hoy tar karon holo if er bitore if use kora hoyeche,chaile amra onek golo eivabe set korte pari.
