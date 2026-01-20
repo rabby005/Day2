423e76f144331685436e3e8ab4e813e6dcf7cc16
rabby005 <farabyabdulla@gmail.com>
2026-01-20 22:33:11 +0600

    day2

--- nesting.py
+++ nesting.py
@@ -2,7 +2,8 @@ age = 34
 
 if(age >= 18):
     print("You are able to car drive.")
-    
+    if(age >= 21):
+        print("You are eligible for retirement benefits.")
 else:
   print("You are kid and not able to all benifits.")
   
